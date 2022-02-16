11ce83a1bd423caa7bcddd5fe1f8260e760bf208
David-coder02 <davidmushi88@gmail.com>
2022-02-16 09:16:15 -0800

    initial commit for the project

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for top ten task"""
+
+
+def top_ten(subreddit):
+    """The function Queries the Reddit API and returns the top 10 hot posts
+    of the given subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
